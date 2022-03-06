72e1f46c2997aa4272dfe94d011d79b242358de1
frootloopers <greenghost96@gmail.com>
2022-03-05 23:58:15 -0500

    added login feature

--- video_classroom/components/Select.tsx
+++ video_classroom/components/Select.tsx
@@ -3,22 +3,29 @@ import styled from 'styled-components';
 
 export interface SelectProps extends React.HTMLAttributes<HTMLSelectElement> {
     values?: string[];
+    setSelected?: (newSelection: string) => void,
 }
 
 export const Select: React.FC<SelectProps> = ({
     values = [],
+    setSelected,
     ...props
 }): React.ReactElement => {
     const mapValues = (values: string[]): React.ReactNode[] => 
         values.map((val, index) => <option key={index}>{val}</option>)
+        
+    const handleChange = (event: { target: { value: string; }; }) => {
+        setSelected && setSelected(event.target.value);
+    }
     
     return (
-        <StyledSelect>
+        <StyledSelect onChange={handleChange}>
             {mapValues(values)}
         </StyledSelect>
 )};
 
 const StyledSelect = styled.select`
+    
     border: 1.5px solid rgba(0,0,0,0.1);
 `
 
